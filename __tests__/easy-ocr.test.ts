6f6d0936b2e3cd68f0ee284f88a4e47a4e04af4a
simone <simonegauli@gmail.com>
2024-07-22 15:52:49 +0200

    fix wrong export after moving to eslint 9

--- __tests__/easy-ocr.test.ts
+++ __tests__/easy-ocr.test.ts
@@ -1,10 +1,14 @@
 import { describe, it } from '@jest/globals';
+import easyOcr from '../src/index';
+import path from 'path';
 
 describe('whisper function', () => {
   it(
     'should transcribe audio successfully',
     async () => {
-      console.log('Implement a test.');
+      const reader = easyOcr.reader('', { pythonPath: '' });
+      const textDetection = await reader.readAsText(path.join('media', 'temp-png'));
+      console.log(textDetection);
     },
     10 * 60 * 1000,
   );
